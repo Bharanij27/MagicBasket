ac31db14f227fbfce0e8465a2389a26f7b1b19e7
Bharanij27 <bhavanisandhiya58@gmail.com>
2021-06-03 10:48:25 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Shop-Basket';
+export class AppComponent {	
+	title = 'Shop-Basket';
+	fruits: string[] = ["apple", "orange", "banana", "grape", "mango", "lemon", "avocado", "pineapple"];
+	vegetables: string[] = ["carrot", "beetroot", "broccoli", "beans", "tomato", "onion", "brinjal"]
+	itemBaskets: string[] = ["Mango"];
+
+	itemInput: string  = "";
+
+	addItemToBasket =  (itemName : string) : void => {
+		let item: string = itemName.trim().toLowerCase();
+		if(this.fruits.includes(item) || this.vegetables.includes(item)  && !this.itemBaskets.includes(item)) {
+			let captilizedItem: string = item.charAt(0).toUpperCase() + item.slice(1, item.length); 
+			this.itemBaskets.push(captilizedItem);
+			this.itemInput =  "";
+		}
+		else{
+			alert("Item not match with the list")
+		}
+	}
+
+	deleteItem = (index: number) : void => {
+		this.itemBaskets.splice(index, 1);
+	}
 }
